c6d6e365f3852c462ca815f298b748dcba5146f1
sax <yusuphshamsondeen@gmail.comx>
2021-07-11 17:23:19 +0100

    💄 eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,22 @@ module.exports = {
 	root: true,
 	extends: [
 		'react-native-typescript',
-		// 'plugin:import/errors',
-		// 'plugin:import/warnings',
+		'plugin:import/errors',
+		'plugin:import/warnings',
 		'plugin:import/typescript',
 	],
 	rules: {
 		indent: 'off', //['error', 'tab'],
 		curly: 'off',
 		semi: ['error', 'never'],
+		// 'import/extensions': 'off',
 	},
 	settings: {
 		'import/resolver': {
 			typescript: {},
+			node: {
+				extensions: ['.js', '.jsx', '.ts', '.tsx'],
+			},
 		},
 	},
 }
