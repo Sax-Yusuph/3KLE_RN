6dc79524d31a4a343276271163307c922ed88b50
sax <yusuphshamsondeen@gmail.comx>
2021-07-12 18:01:11 +0100

    changed splash header

--- src/components/views/onBoarding/extra/data.tsx
+++ src/components/views/onBoarding/extra/data.tsx
@@ -1,27 +1,64 @@
+import { AppLogo, Heading, HStack } from '@elements'
+import React from 'react'
+
 const Data = [
 	{
+		Heading: (
+			<HStack justifyContent='flex-end' mb='lg'>
+				<AppLogo />
+			</HStack>
+		),
 		title: `Open your bank account in minutes. Link your debit and credit cards from other banks and grow your money with us.`,
 		image: require('@assets/png/splash.png'),
 	},
 	{
+		Heading: (
+			<HStack justifyContent='flex-end' mb='lg'>
+				<AppLogo />
+				<Heading color='card' mx='lg'>
+					FAMILY
+				</Heading>
+				<Heading color='card'>PlAN</Heading>
+			</HStack>
+		),
 		title: `Open your bank account in minutes. Link your debit and credit cards from other banks and grow your money with us.`,
-		image: require('@assets/png/spalsh-screen-2.png'),
+		image: require('@assets/png/splash_screen_2.png'),
 	},
 	{
+		Heading: (
+			<HStack justifyContent='flex-end' mb='lg'>
+				<AppLogo />
+			</HStack>
+		),
 		title: `Open your bank account in minutes. Link your debit and credit cards from other banks and grow your money with us.`,
-		image: require('@assets/png/splash-screen-3.png'),
+		image: require('@assets/png/splash_screen_3.png'),
 	},
 	{
+		Heading: (
+			<HStack justifyContent='flex-end' mb='lg'>
+				<AppLogo />
+			</HStack>
+		),
 		title: `Open your bank account in minutes. Link your debit and credit cards from other banks and grow your money with us.`,
-		image: require('@assets/png/splash-screen-4.png'),
+		image: require('@assets/png/splash_screen_4.png'),
 	},
 	{
+		Heading: (
+			<HStack justifyContent='flex-end' mb='lg'>
+				<AppLogo />
+			</HStack>
+		),
 		title: `Open your bank account in minutes. Link your debit and credit cards from other banks and grow your money with us.`,
-		image: require('@assets/png/splash-screen-5.png'),
+		image: require('@assets/png/splash_screen_5.png'),
 	},
 	{
+		Heading: (
+			<HStack justifyContent='flex-end' mb='lg'>
+				<AppLogo />
+			</HStack>
+		),
 		title: `Our Family plus account allow parents to save money and invest and maintain full control until their child is an adult.`,
-		image: require('@assets/png/splash-screen-6.png'),
+		image: require('@assets/png/splash_screen_6.png'),
 	},
 ]
 
--- src/components/views/onBoarding/index.tsx
+++ src/components/views/onBoarding/index.tsx
@@ -104,9 +104,7 @@ export default () => {
 							>
 								{Data.map((slideItem, index) => (
 									<Center key={slideItem.title + index}>
-										<HStack justifyContent='flex-end' mb='lg'>
-											<AppLogo />
-										</HStack>
+										{slideItem.Heading}
 										<Slide title={slideItem.title} {...{ Indicator, index }} />
 									</Center>
 								))}
